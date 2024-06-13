a492cd1158385c8531116469e2ace3214e403a71
unknown <ridoymahmud678@gmail.com>
2024-06-13 19:22:44 +0600

    Updated

--- src/healthconsultant/HealthConsultant.java
+++ src/healthconsultant/HealthConsultant.java
@@ -4,7 +4,7 @@ package healthconsultant;
 //This is the main class to run the code
 
 public class HealthConsultant {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         test ob = new test();
         ob.setVisible(true);
         ob.setResizable(false);
